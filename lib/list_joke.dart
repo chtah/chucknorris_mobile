25bdf6350c7dfd39a14907653f04332deeff2a15
chtah <chawakorn_ha@hotmail.com>
2023-12-22 00:09:37 +0700

    add border color

--- lib/list_joke.dart
+++ lib/list_joke.dart
@@ -258,6 +258,10 @@ class _ListJoke extends State<ListJoke> with WidgetsBindingObserver {
                           );
                         },
                         child: Card(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              side: const BorderSide(
+                                  color: Color.fromARGB(255, 241, 90, 36))),
                           child: Stack(
                             children: [
                               Column(
